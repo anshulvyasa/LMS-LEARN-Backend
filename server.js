115489a1f27be5d2ef6b3e39253615c7f5d88559
anshulvyasa <anshulvtech121@gmail.com>
2025-01-03 17:57:08 +0530

    instructor-course-model controller and routes are added

--- server.js
+++ server.js
@@ -1,56 +1,52 @@
-require('dotenv').config();
-const express=require('express')
-const cors=require('cors')
-const mongoose=require('mongoose')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 //Routes
-const authRoutes=require('./routes/auth-routes/index')
-const mediaRoutes=require('./routes/instructor-routes/media-routes')
+const authRoutes = require("./routes/auth-routes/index");
+const mediaRoutes = require("./routes/instructor-routes/media-routes");
+const instructorCourseRoutes = require("./routes/instructor-routes/course-routes");
 
-const app=express();
-const PORT=process.env.PORT||5000;
-const MONGO_URL=process.env.MONGO_URL
+const app = express();
+const PORT = process.env.PORT || 5000;
+const MONGO_URL = process.env.MONGO_URL;
 
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  methods: ["GET", "POST", "DELETE", "PUT"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
-const corsOptions={
-    origin:process.env.CLIENT_URL,
-    methods: ["GET", "POST", "DELETE", "PUT"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  }
-
-app.use(
-    cors(corsOptions)
-  );
-
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
 //database connection
 mongoose
-.connect(MONGO_URL)
-.then(()=>console.log("Database is Connected"))
-.catch((e)=> console.log(e.message));
+  .connect(MONGO_URL)
+  .then(() => console.log("Database is Connected"))
+  .catch((e) => console.log(e.message));
 
-app.get('/',(req,res)=>{
-    res.json({
-        msg:"hey dude"
-    })
-})
+app.get("/", (req, res) => {
+  res.json({
+    msg: "hey dude",
+  });
+});
 
 //rotes configuration
-app.use('/auth',authRoutes);
-app.use('/media',mediaRoutes);
+app.use("/auth", authRoutes);
+app.use("/media", mediaRoutes);
+app.use("/instructor/course", instructorCourseRoutes);
 
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500).json({
+    success: false,
+    message: "Something went wrong",
+  });
+});
 
-
-app.use((err,req,res,next)=>{
-    console.log(err.stack);
-    res.status(500).json({
-        success:false,
-        message:'Something went wrong'
-    })
-})
-
-app.listen(PORT,()=>{
-    console.log(`Server is Listening at Port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is Listening at Port ${PORT}`);
+});
