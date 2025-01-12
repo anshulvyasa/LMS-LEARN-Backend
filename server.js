eb50739f4866b1440c929d70f89f400ec3d244b9
anshulvyasa <anshulvtech121@gmail.com>
2025-01-12 17:05:26 +0530

    Paypal Payment config payment_json and order controller and routes are added

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const authRoutes = require("./routes/auth-routes/index");
 const mediaRoutes = require("./routes/instructor-routes/media-routes");
 const instructorCourseRoutes = require("./routes/instructor-routes/course-routes");
 const StudentViewCourseRoutes = require("./routes/student-routes/course-routes");
+const StudentViewOrderRoutes = require("./routes/student-routes/order-route");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -40,6 +41,7 @@ app.use("/auth", authRoutes);
 app.use("/media", mediaRoutes);
 app.use("/instructor/course", instructorCourseRoutes);
 app.use("/student/course", StudentViewCourseRoutes);
+app.use("/student/order", StudentViewOrderRoutes);
 
 app.use((err, req, res, next) => {
   console.log(err.stack);
